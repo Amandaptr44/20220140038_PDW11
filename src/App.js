6eb0f4e06adb544054d3092fd16d7015ad26233a
github_Amandaptr44 <amanda.putri.ft22@mail.umy.ac.id>
2024-06-13 18:40:41 +0700

    20220140038_PDW11

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './Poppin.gif';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>Haii</code> Bahagia selaluu!
         </p>
         <a
           className="App-link"
